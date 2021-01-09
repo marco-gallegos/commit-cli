fd34a4b000a851709adb9da9f1ed8dcb0c8dc617
marco-gallegos <ma_galeza@hotmail.com>
2021-01-09 03:35:02 -0600

    [IMP] config manager: loading, saving file
    this update contains the first stable implementation in the saving and loading file feature

--- configmanager/config.py
+++ configmanager/config.py
@@ -3,24 +3,16 @@
 @Date   2020/12/31
 @Update 2020/12/31
 @Description
-    
+    Configuration is a class to contain and validate the configuration
 """
+import os
 
 class Configuration(object):
-    """
-
-    Args:
-        object ([type]): [description]
-    """
     def __init__(self, config:dict=None, signgpg:bool=False):
         self.config = {
             'format': "odoo",
             'signgpg': signgpg,
         }
-
-        if config:
-            self.config['format'] = config['format'] if 'format' in config else self.config['format']
-            self.config['signgpg'] = config['signgpg'] if 'format' in config[] else self.config['signgpg']
 
         self.supported_formats = [
             "odoo",
@@ -29,5 +21,45 @@ class Configuration(object):
             "free",
         ]
 
-        def __str__(self):
-            return f"configuracion|  format: {self.config['format']} || sign: {self.config['signgpg']}"
+        if config:
+            self.config['format'] = config['format'].lower() if 'format' in config else self.config['format']
+            self.config['signgpg'] = config['signgpg'] if 'signgpg' in config else self.config['signgpg']
+            self.config['format'] = self.config['format'].lower()
+
+            if self.config['signgpg'] and not self.can_sign_gpg():
+                print("Tu configuracion solicita firmar commits pero git no esta configurado")
+                self.config['signgpg']=False
+            if not (self.config['format'] in self.supported_formats):
+                print(f"el formato {self.config['format']} no es soportado, por defecto usaremos el formato de {self.supported_formats[0]}")
+                self.config['format']=self.supported_formats[0]
+
+        self.can_sign_gpg()
+
+    
+    def __str__(self)->str:
+        return f"configuracion|  format: {self.config['format']} || sign: {self.config['signgpg']}"
+
+    
+    def can_sign_gpg(self)->bool:
+        signkey = os.popen("git config --global user.signingkey").read() 
+        if not signkey :
+            return False
+        else:
+            return True
+
+
+    def is_a_valid_format(self, format:str)->bool:
+        return format in self.supported_formats
+
+
+    def get_configuration_file_string(self, filled:bool=True)->str:
+        file_template = "#Format for every commit\n"\
+        "#supported formats free, odoo, sgc(semantic git commits) and cc (conventional commits)\n"\
+        "format={format}\n\n"\
+        "#Option to sign the commits o every commit, must be True or False\n"\
+        "signgpg={sign}"\
+        
+        if filled:
+            return file_template.format(format=self.config['format'], sign=self.config['signgpg'])
+        else:
+            return file_template
--- configmanager/config_manager.py
+++ configmanager/config_manager.py
@@ -3,7 +3,7 @@
 @Date   2020/12/31
 @Update 2020/12/31
 @Description
-    
+    The config manager is a class to manage the configuration file by default in ~/.commitclirc
 """
 import os, pathlib
 from configmanager.config import Configuration
@@ -23,7 +23,8 @@ class ConfigManager(object):
         self.pattern_regex_clave_valor = re.compile(self.regex_clave_valor)
         self.init_config()
 
-    def current_file(self):
+
+    def current_file(self)->str:
         return f"{pathlib.Path.home()}/{self._file}"
     
 
@@ -31,16 +32,18 @@ class ConfigManager(object):
         exist = os.path.exists(self.current_file())
         return True if exist else False
 
+
     def stringline_to_key_value(self, string_line:str="#comentario")->str:
         key, value = None, None
         match = re.fullmatch(self.pattern_regex_clave_valor, string_line)
-        if match and type(string_line) == str:
+        if match and type(string_line) == str and string_line:
             elements = string_line.split("=")
             key= elements[0]
             value= elements[1]
         return key,value
 
-    def load_file(self):
+
+    def load_file(self)->dict:
         if self.exist_file():
             data_dict = {}
             file= open(self.current_file(), 'r')
@@ -51,18 +54,25 @@ class ConfigManager(object):
                 key, value = self.stringline_to_key_value(string_line=file_line)
                 if value == "False" or value == "True" or value == "false" or value == "true":
                     value = bool(distutils.util.strtobool(value.lower()))
-                data_dict[key] = value
+                #print(f"{key} {value}")
+                if key != None and value != None:
+                    data_dict[key] = value
             return data_dict
+        else:
+            return None
 
     
-    def save_file(self):
-        pass
+    def save_file(self)->bool:
+        file= open(self.current_file(), 'w')
+        file.write(self.config.get_configuration_file_string())
+        file.close()
+        return True
 
 
     def init_config(self)->bool:
         if self.exist_file():
             data = self.load_file()
-            print(data)
+            #print(data)
             self.config = Configuration(config=data)
             return True
         else:
