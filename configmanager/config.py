4f009aeffa03876ee6c7819013d68a5b0b8e329c
marco-gallegos <ma_galeza@hotmail.com>
2021-01-09 11:52:43 -0600

    [IMP] config manager: docstrings
    added docstring for the config class

--- configmanager/config.py
+++ configmanager/config.py
@@ -37,10 +37,21 @@ class Configuration(object):
 
     
     def __str__(self)->str:
+        """Return a string representation of the object, this function
+        is called by the built in print function
+
+        :return: string representation
+        :rtype: str
+        """
         return f"configuracion|  format: {self.config['format']} || sign: {self.config['signgpg']}"
 
     
     def can_sign_gpg(self)->bool:
+        """Return true if the users git repository is configured to sign the commits
+
+        :return: indicates that the user can sign the commits
+        :rtype: bool
+        """
         signkey = os.popen("git config --global user.signingkey").read() 
         if not signkey :
             return False
@@ -49,10 +60,25 @@ class Configuration(object):
 
 
     def is_a_valid_format(self, format:str)->bool:
+        """Return true if the format parameter string is a supported commit format
+
+        :param format: format to evaluate
+        :type format: str
+        :return: boolean if the format is supported
+        :rtype: bool
+        """
         return format in self.supported_formats
 
 
     def get_configuration_file_string(self, filled:bool=True)->str:
+        """This method returns the string to store the configuration file,
+        optionally this function return the string template
+
+        :param filled: if you want the filled string or the template string, defaults to True
+        :type filled: bool, optional
+        :return: format string
+        :rtype: str
+        """
         file_template = "#Format for every commit\n"\
         "#supported formats free, odoo, sgc(semantic git commits) and cc (conventional commits)\n"\
         "format={format}\n\n"\
