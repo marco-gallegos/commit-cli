436ce0157cd3df11832be88112c82bd6080ebc71
marco-gallegos <ma_galeza@hotmail.com>
2023-09-01 17:34:05 -0600

    chore(comitcli): updated get id func

--- common/functions.py
+++ common/functions.py
@@ -48,7 +48,7 @@ def get_first_commit_id(repo_path: str):
         return None  # No se encontró la rama principal
 
     # Obtener el primer commit en la rama principal
-    first_commit = next(repo.walk(main_branch_ref.target, pygit2.GIT_SORT_TOPOLOGICAL))
+    first_commit = next(repo.walk(main_branch_ref.target, pygit2.GIT_SORT_REVERSE))
 
     # Devolver el ID del primer commit como un string hexadecimal
     return first_commit.id.hex
